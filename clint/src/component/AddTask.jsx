406030c7804edf1089cf6f00af3188a94aae4424
Md-Asif-Azmi01 <asifazmi3549@gmail.com>
2026-01-09 05:03:28 +0530

    Update AddTask.jsx

--- clint/src/component/AddTask.jsx
+++ clint/src/component/AddTask.jsx
@@ -20,7 +20,7 @@ const AddTask = () => {
     e.preventDefault();
     console.log("Sending data:", Value);
     try {
-      const res = await axios.post("https://taskmanagerbackend-evoe.onrender.com/api/vi/addtask", Value);
+      const res = await axios.post("https://taskmanagerbackend-evoe.onrender.com", Value);
       console.log("Response:", res.data);
       alert("Task added successfully!");
       Navigate("/");
